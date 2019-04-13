6fa44f3ed1fe9975139da2fd43d0cb2f6b774a9b
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-13 13:08:38 +0900

    [fix] メールアドレス修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
     # mail to: "#{contact.user.email}",
-    mail to: "oyaken-0717@yahoo.co.jp",
+    mail to: "#{contact.user.email}",
     subject: "お問い合わせの確認メール"
   end
 end
